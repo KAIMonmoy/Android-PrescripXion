ba1cc4b2687c5053e2f5e893021ac2217db0bd45
KAIMonmoy <aftarulislam@gmail.com>
2018-01-05 13:40:00 +0600

    Module 4 Rechecked

--- app/src/main/java/com/prescripxion/www/prescripxion2module/CameraActivity.java
+++ app/src/main/java/com/prescripxion/www/prescripxion2module/CameraActivity.java
@@ -1,5 +1,6 @@
 package com.prescripxion.www.prescripxion2module;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -9,11 +10,11 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -27,83 +28,72 @@ public class CameraActivity extends AppCompatActivity {
     ImageView imageView;
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Now user should be able to use camera
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                String pictureName = getPictureName();
-
-                File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/PrescripXion");
-                if (!path.exists()) {
-                    path.mkdirs();
-                }
-                File image = new File(path, pictureName);
-                Uri pictureUri = Uri.fromFile(image);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            } else {
-                // Your app will not have this permission. Turn off all functions
-                // that require this permission or it will force close like your
-                // original question
-            }
-        }
-    }
-
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
         imageView = findViewById(R.id.imageView);
 
-            //if( ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-                   // != PackageManager.PERMISSION_GRANTED) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION);
-                }
-                else{
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                    String pictureName = getPictureName();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                invokeCamera();
+            } else{
+                String[] requestedPermission = {Manifest.permission.CAMERA};
+                requestPermissions(requestedPermission, MY_CAMERA_PERMISSION);
+            }
+        }
+        else{
+            invokeCamera();
+        }
 
-                    File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/PrescripXion");
-                    if (!path.exists()) {
-                        path.mkdirs();
-                    }
-                    File image = new File(path, pictureName);
-                    Uri pictureUri = Uri.fromFile(image);
-                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
-            //}
+        Button openCameraButton = (Button) findViewById(R.id.camera);
 
-            Button openCameraButton = (Button) findViewById(R.id.camera);
-
-            openCameraButton.setOnClickListener(new View.OnClickListener() {
+        openCameraButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION);
+                        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                            invokeCamera();
+                        } else{
+                            String[] requestedPermission = {Manifest.permission.CAMERA};
+                            requestPermissions(requestedPermission, MY_CAMERA_PERMISSION);
+                        }
                     }
                     else{
-                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                        String pictureName = getPictureName();
-
-                        File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/PrescripXion");
-                        if (!path.exists()) {
-                            path.mkdirs();
-                        }
-                        File image = new File(path, pictureName);
-                        Uri pictureUri = Uri.fromFile(image);
-                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                        invokeCamera();
                     }
                 }
-            });
+        });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Now user should be able to use camera
+                invokeCamera();
+            } else {
+                // App will not have this permission. Turn off all functions
+                Toast.makeText(this, "Camera permission not granted.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void invokeCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        String pictureName = getPictureName();
+
+        File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/PrescripXion");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        File image = new File(path, pictureName);
+        Uri pictureUri = Uri.fromFile(image);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
 
